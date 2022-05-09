70f5097b14b2ad8769bf87a5a8312dffd61f4d6f
Stefan Mitov <stefanmitov8@gmail.com>
2022-05-09 20:34:40 +0300

    calculate time to complete all payments

--- client/index.js
+++ client/index.js
@@ -2,8 +2,21 @@ const axios = require('axios');
 
 let makeRequest = async (id) => {
     const amount = Math.ceil(Math.random() * 5000);
-    const result = await axios.post(`http://localhost:14803/api/card/amount?amount=${amount}`);
+    const result = await axios.post(
+        `http://localhost:14803/api/card/amount?amount=${amount}`
+    );
     console.log(`${id} - сума: $${amount} - ${result.data}`);
 };
 
-for (let i = 1; i <= 5; i++) makeRequest(i);
+calculateTime = async (threads) => {
+    const start = Date.now();
+
+    await Promise.all(
+        [...Array(threads).keys()].map(makeRequest)
+    );
+
+    const end = Date.now() - start;
+    console.log(`Elapsed time: ${end / 1000} seconds`);
+}
+
+calculateTime(20);
